dbd5f84fa11f05a503c3f74359bd4441fc3a97d9
Alberto Moral <alberto.moral.g@gmail.com>
2022-06-22 09:10:29 +0200

    Initial Commit

---
+++ WallaMarvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WallaMarvel
+//
+//  Created by Alberto Moral on 22/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
