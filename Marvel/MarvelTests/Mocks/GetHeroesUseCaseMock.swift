6888592de674f39d3c3f2c6968c64e6307391556
Sergio David Bravo Talero <sergiobtalero@aol.com>
2025-03-31 11:29:24 +0200

    Unit tests of heroes list view model

---
+++ Marvel/MarvelTests/Mocks/GetHeroesUseCaseMock.swift
@@ -0,0 +1,32 @@
+//
+//  GetHeroesUseCaseMock.swift
+//  MarvelTests
+//
+//  Created by Sergio David Bravo Talero on 31/3/25.
+//
+
+import Domain
+import Foundation
+
+final class GetHeroesUseCaseMock: GetHeroesUseCaseProtocol {
+    var response: CharacterDataContainer
+    var nextResponse: CharacterDataContainer?
+    var executeCallCount = 0
+    
+    init(response: CharacterDataContainer) {
+        self.response = response
+    }
+    
+    convenience init() {
+        let characters = [
+            CharacterDataModel(id: 1, name: "Spider Man", description: "Spider Man", thumbnail: Thumbnail(path: "path", extension: "extension")),
+            CharacterDataModel(id: 2, name: "Hulk", description: "Hulk", thumbnail: Thumbnail(path: "path", extension: "extension"))
+        ]
+        self.init(response: CharacterDataContainer(count: 10, limit: 100, offset: 0, total: 100, characters: characters))
+    }
+
+    func execute(page: Int) async throws -> CharacterDataContainer {
+        executeCallCount += 1
+        return nextResponse ?? response
+    }
+}
---
+++ Marvel/MarvelTests/ViewModels/HeroesListViewModelTests.swift
@@ -0,0 +1,91 @@
+//
+//  HeroesListViewModelTests.swift
+//  MarvelTests
+//
+//  Created by Sergio David Bravo Talero on 31/3/25.
+//
+
+import Domain
+import Testing
+@testable import Marvel
+
+struct HeroesListViewModelTests {
+    @Test
+    func loadFirstPage_setsHeroes() async throws {
+        let getHeroesUseCaseMock = GetHeroesUseCaseMock()
+        
+        let sut = HeroesListViewModel(getHeroesUseCase: getHeroesUseCaseMock)
+        
+        await sut.loadFirstPage()
+        
+        #expect(sut.heroes.count == 2)
+        #expect(sut.heroes.first?.name == "Spider Man")
+    }
+    
+    @Test
+    func loadNextPage_appendsHeroes() async throws {
+        let initialCharacters = [
+            CharacterDataModel(id: 1, name: "Spider Man", description: "Spider Man", thumbnail: Thumbnail(path: "path", extension: "extension"))
+        ]
+        let response = CharacterDataContainer(count: 100, limit: 100, offset: 0, total: 100, characters: initialCharacters)
+        let nextCharacters = [
+            CharacterDataModel(id: 2, name: "Hulk", description: "Hulk", thumbnail: Thumbnail(path: "path", extension: "extension"))
+        ]
+        let nextResponse = CharacterDataContainer(count: 100, limit: 100, offset: 0, total: 100, characters: nextCharacters)
+        
+        let getHeroesUseCaseMock = GetHeroesUseCaseMock(response: response)
+        let viewModel = HeroesListViewModel(getHeroesUseCase: getHeroesUseCaseMock)
+        
+        await viewModel.loadFirstPage()
+        
+        getHeroesUseCaseMock.nextResponse = nextResponse
+        await viewModel.loadNextPage()
+        
+        #expect(viewModel.heroes.count == 2)
+        #expect(viewModel.heroes.last?.name == "Hulk")
+    }
+    
+    @Test
+    func didSelectHero_setsSelectedHero() {
+        let getHeroesUseCaseMock = GetHeroesUseCaseMock()
+        let viewModel = HeroesListViewModel(getHeroesUseCase: getHeroesUseCaseMock)
+        let hero = CharacterDataModel(id: 1, name: "Spider Man", description: "Spider Man", thumbnail: Thumbnail(path: "path", extension: "extension"))
+        viewModel.didSelectHero(hero)
+        
+        #expect(viewModel.heroSelected == hero)
+    }
+    
+    @Test
+    func query_filtersResults() async throws {
+        let getHeroesUseCaseMock = GetHeroesUseCaseMock()
+        let viewModel = HeroesListViewModel(getHeroesUseCase: getHeroesUseCaseMock)
+        
+        await viewModel.loadFirstPage()
+        
+        viewModel.query = "Spider"
+        
+        try await Task.sleep(for: .milliseconds(100))
+        
+        #expect(viewModel.heroes.count == 1)
+        #expect(viewModel.heroes.first?.name == "Spider Man")
+    }
+    
+    @Test
+    func loadNextPage_doesNotTrigger_whenQueryIsNotEmpty() async throws {
+        let initialCharacters = [
+            CharacterDataModel(id: 1, name: "Spider Man", description: "Spider Man", thumbnail: Thumbnail(path: "path", extension: "extension"))
+        ]
+        let response = CharacterDataContainer(count: 100, limit: 100, offset: 0, total: 100, characters: initialCharacters)
+        let getHeroesUseCaseMock = GetHeroesUseCaseMock(response: response)
+        let viewModel = HeroesListViewModel(getHeroesUseCase: getHeroesUseCaseMock)
+        
+        await viewModel.loadFirstPage()
+        
+        viewModel.query = "Iron"
+        try await Task.sleep(for: .milliseconds(100))
+        
+        await viewModel.loadNextPage()
+        
+        #expect(getHeroesUseCaseMock.executeCallCount == 1)
+    }
+}
